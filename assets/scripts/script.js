81d0cdae5c98c907e2db140bb73f75f634bed69d
Henrique Luiz <hnrq.luiz1@gmail.com>
2021-06-18 19:32:17 -0300

    Carousel button & header bc-color transition

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+const headerMenu = document.querySelector('.header-row');
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > headerMenu.clientHeight) {
+        headerMenu.style.background = "black"
+    } else {
+        headerMenu.style.background = "none"
+    }
+})
